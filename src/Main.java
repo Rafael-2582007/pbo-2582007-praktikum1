07f213aa299acbfc63d98342ccab24348ebb373c
Rafael-2582007 <2582007@unai.edu>
2026-08-31 22:28:10 +0700

    Menambahkan String Biodata

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Biodata {
     public static void main(String[] args) {
 
+        String nama = "Rafael Alfaro Ataupah";
+        String nim  = "2582007";
+        String prodi  = "Sistem Informasi";
+        String Status  = "Insight";
+        String Target_Semester  = "Bisa belajar Java dan mendalami database.";
+        String Target_IPK  = "3.85";
+
     }
 }
