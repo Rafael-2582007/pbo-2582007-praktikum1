86a719054c246b0e09eaf967067155ceb6eca0e8
Rafael-2582007 <2582007@unai.edu>
2026-08-31 22:29:41 +0700

    Output dari Biodata

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,13 @@ public class Biodata {
         String Target_Semester  = "Bisa belajar Java dan mendalami database.";
         String Target_IPK  = "3.85";
 
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM  : " + nim);
+        System.out.println("Prodi  : " + prodi);
+        System.out.println("Status : " + Status);
+        System.out.println("Target_Semester  : " + Target_Semester);
+        System.out.println("Target_IPK  : " + Target_IPK);
+
+
     }
 }
