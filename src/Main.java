98f6b5022c93c8ad5f1adf5f52075c7c8306c229
Rafael-2582007 <2582007@unai.edu>
2026-08-31 22:14:59 +0700

    biodata

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Biodata {
+    public static void main(String[] args) {
+        String nama = "Rafael Alfaro Ataupah";
+        String nim  = "2582007";
+        String prodi  = "Sistem Informasi";
+        String Status  = "Insight";
+        String Target_Semester  = "Bisa belajar Java dan mendalami database.";
+        String Target_IPK  = "3.85";
+
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM  : " + nim);
+        System.out.println("Prodi  : " + prodi);
+        System.out.println("Status : " + Status);
+        System.out.println("Target_Semester  : " + Target_Semester);
+        System.out.println("Target_IPK  : " + Target_IPK);
+    }
+}
